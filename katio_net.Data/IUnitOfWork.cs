eea76ec7d873c54af3dc6a333ff25ac31396f9de
Daniel Orrego <josedanielorregor@gmail.com>
2024-10-24 20:16:18 -0500

    It just changed location, nothing changed

--- katio_net.Data/IUnitOfWork.cs
+++ katio_net.Data/IUnitOfWork.cs
@@ -4,13 +4,10 @@ using katio.Data;
 namespace katio.Data;
 public interface IUnitOfWork
 {
-    
-    IRepository<int, Genre> GenreRepository { get; }
-    IRepository<int, Narrator> NarratorRepository { get; } 
     IRepository<int, Book> BookRepository { get; }
     IRepository<int, Author> AuthorRepository { get; }
     IRepository<int, AudioBook> AudioBookRepository { get; }
- 
+    IRepository<int, Genre> GenreRepository { get; }
+    IRepository<int, Narrator> NarratorRepository { get; }
     Task SaveAsync();
-
 }
